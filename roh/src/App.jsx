639761a24a35b0d7f24694d8895c13df4e2358e9
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2023-01-28 11:56:38 +0000

    set up hosting and api call

--- roh/src/App.jsx
+++ roh/src/App.jsx
@@ -1,11 +1,39 @@
 import "./App.scss";
 import CreativeList from "./containers/CreativeList/CreativeList";
+import { useState, useEffect } from "react";
 
 function App() {
+  // const [performanceDetails, setPerformanceDetails] = useState([]);
+
+  // const getPerformance = async () => {
+  //   const url =
+  //     "https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban";
+  //   const res = await fetch(url);
+  //   const data = await res.json();
+  //   console.log(data);
+  // };
+  useEffect(() => {
+    fetch(
+      "https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban",
+      {
+        method: "get",
+        headers: { Accept: "*/*" },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("error");
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <CreativeList />
+      <p></p>
     </div>
   );
 }
