af7d82f3bdc7cd58e3f37972f9d59315f4117288
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2023-01-29 14:19:27 +0000

    add a test

--- roh/src/App.test.js
+++ roh/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("initial test for react card", () => {
+  it("should render headings", () => {
+    render(<App />);
+    const headings = screen.getAllByRole("heading");
+    headings.forEach((heading) => {
+      expect(heading).toBeInTheDocument();
+    });
+    headings.forEach((heading) => {
+      expect(heading).not.toBeNull();
+    });
+  })
+})
